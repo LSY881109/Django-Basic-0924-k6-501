11727b26a05e947efe1a71e8acddbce05e842a08
이상용 <lsy3709@naver.com>
2025-09-24 16:05:28 +0900

    7교시, 파이버거, 데이터베이스 와 화면연동 확인

--- pyburger/pyburger/views.py
+++ pyburger/pyburger/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from burgers.models import Burger
+
 
 def main(request):
     # return HttpResponse("Hello, world. 파이버거~~")
@@ -8,4 +10,7 @@ def main(request):
 
 def burger_list(request):
     # return HttpResponse("파이버거의 햄버거 목록입니다.~~~")
-    return render(request, 'burger_list.html')
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록:", burgers)
+    context = {'burgers': burgers}
+    return render(request, 'burger_list.html', context)
